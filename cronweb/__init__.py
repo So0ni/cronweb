93ee62205a5bff9163844ae23e70466bf7ebfd7b
So0ni <173976914@qq.com>
2021-06-09 00:00:40 +0800

    尝试在非windows平台使用uvloop

--- cronweb/__init__.py
+++ cronweb/__init__.py
@@ -15,6 +15,13 @@ import time
 
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+else:
+    try:
+        import uvloop
+    except (ImportError, ModuleNotFoundError):
+        pass
+    else:
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
 class CronWeb:
