5453648f34e38b116b5a19e01d615340118fc714
So0ni <173976914@qq.com>
2021-06-11 23:13:17 +0800

    添加systemd service文件生成

--- install.py
+++ install.py
@@ -32,8 +32,6 @@ def check_secure():
 
 def generate_config_file():
     print('从模板生成配置文件...')
-    from pathlib import Path
-    dir_project = Path(__file__).parent
     file_tmpl_config = dir_project / 'template' / 'config.yaml.tmpl'
     with open(file_tmpl_config, 'r', encoding='utf8') as fp:
         tmpl_config = fp.read()
@@ -67,9 +65,13 @@ def generate_config_file():
 def create_venv():
     print('生成虚拟环境...')
     import venv
-    from pathlib import Path
-    dir_project = Path(__file__).parent
+    global dir_venv, bin_python
     dir_venv = dir_project / '.venv'
+    if system == 'Windows':
+        bin_python = dir_venv / 'Scripts' / 'python.exe'
+    else:
+        bin_python = dir_venv / 'bin' / 'python'
+
     if dir_venv.exists():
         print('虚拟环境已存在，跳过生成')
         return None
@@ -79,19 +81,31 @@ def create_venv():
 def install_pkg():
     print('安装必要依赖...')
     import subprocess
-    import platform
-    from pathlib import Path
-    dir_project = Path(__file__).parent
-    dir_venv = dir_project / '.venv'
-    if platform.system() == 'Windows':
-        bin_python = dir_venv / 'Scripts' / 'python.exe'
-    else:
-        bin_python = dir_venv / 'bin' / 'python'
-    if bin_python.exists():
+    if not bin_python.exists():
         print(f'{bin_python}不存在 退出')
         sys.exit(3)
     file_req = dir_project / 'requirements.txt'
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', str(file_req)])
+    subprocess.check_call([str(bin_python), '-m', 'pip', 'install', '-r', str(file_req)])
+
+
+def generate_env_subprocess():
+    print('生成运行环境配置文件...')
+    import json
+    env_source = dict(os.environ)
+    env_source['VIRTUAL_ENV'] = str(dir_venv)
+    env_source.pop('PYTHONHOME', None)
+    if system == 'Windows':
+        env_source['PATH'] = f"{bin_python.parent};{env_source['PATH']}"
+    else:
+        env_source['PATH'] = f"{bin_python.parent}:{env_source['PATH']}"
+    path_env_file = dir_project / '.env_subprocess.json'
+    if path_env_file.exists():
+        yes_or_no = input('环境文件似乎已经存在，需要替换吗?(yes, no): ').strip().lower()
+        if yes_or_no != 'yes':
+            print(f'你的回答并非yes, 跳过环境文件替换')
+            return None
+    with open(path_env_file, 'w', encoding='utf8') as fp:
+        json.dump(env_source, fp, ensure_ascii=False, indent=4)
 
 
 def common_operation():
@@ -100,6 +114,7 @@ def common_operation():
     generate_config_file()
     create_venv()
     install_pkg()
+    generate_env_subprocess()
 
 
 # 系统操作
@@ -113,6 +128,11 @@ def before_linux():
             import venv
         except ModuleNotFoundError:
             failed_list.append('venv')
+            pass
+        try:
+            import pip
+        except ModuleNotFoundError:
+            failed_list.append('pip')
             pass
         if failed_list:
             print(f'当前Python环境没有安装{failed_list}，请安装后重试')
@@ -132,7 +152,43 @@ def before_macos():
 # 善后
 
 def after_linux():
-    pass
+    def check_systemd():
+        import subprocess
+        out = subprocess.check_output(['ps', '--no-headers', '-o', 'comm', '1']).decode().strip()
+        if 'systemd' not in out:
+            print('当前系统并未使用systemd，不需要生成service文件')
+            sys.exit(3)
+
+    def generate_service_unit():
+        file_tmpl_service = dir_project / 'template' / 'cronweb.service.tmpl'
+        with open(file_tmpl_service, 'r', encoding='utf8') as fp:
+            tmpl_service = fp.read()
+        global user, group
+        user = input('输入用于运行CronWeb的用户的用户名(默认为cronweb): ').strip() or 'cronweb'
+        group = user
+        command = f'{bin_python.absolute()} {dir_project.absolute() / "manage.py"} run'
+        service_str = tmpl_service.format(user=user, group=group, exec=command)
+        file_service = dir_project / 'cronweb.service'
+        with open(file_service, 'w', encoding='utf8') as fp:
+            fp.write(service_str)
+
+        try:
+            import pwd
+            pwd.getpwnam(user)
+        except KeyError:
+            yes_or_no = input(f'用户{user}不存在，你想要创建吗?你需要有对应的sudo权限(yes, no)').strip().lower()
+            if yes_or_no == 'yes':
+                os.system(f'sudo useradd {user}')
+                import getpass
+                current_group = getpass.getuser()
+                os.system(f'sudo usermod -a -G {current_group} {user}')
+            else:
+                print('你的回答并非yes，跳过用户创建，你需要手动创建用户并处理对应日志文件的读写权限')
+            pass
+        print('你需要手动把service文件复制到systemd的目录中，并重载systemd')
+
+    check_systemd()
+    generate_service_unit()
 
 
 def after_windows():
@@ -145,7 +201,12 @@ def after_macos():
 
 def route():
     import platform
+    from pathlib import Path
+
+    global system, dir_project
     system = platform.system()
+    dir_project = Path(__file__).parent
+
     if system == 'Linux':
         before_linux()
         common_operation()
